02c0cf41898b91157f09d795002af2475a23c789
Torben Buck <dev@tbuck.de>
2024-12-20 12:19:51 +0100

    ✨ integrate tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#64748b',
+      },
+    },
+  },
+  plugins: [],
+} 
