0f2ed8387cb4cd5a149ab620ad647d303442525f
Torben Buck <dev@tbuck.de>
2025-12-16 12:00:18 +0100

    feat: add support for previewing EML attachments in the message content renderer
    - Implemented functionality to recognize and render EML file types in the attachment modal.
    - Added a new icon for EML files to enhance user experience and visual consistency with other attachment types.
    - Updated the rendering logic to accommodate the new EML preview feature, improving the overall attachment handling capabilities.

--- src/js/ui/MessageContentRenderer.js
+++ src/js/ui/MessageContentRenderer.js
@@ -210,6 +210,7 @@ export class MessageContentRenderer {
         const isImage = this.attachmentModal?.isPreviewableImage(attachment.attachMimeTag);
         const isPdf = this.attachmentModal?.isPdf(attachment.attachMimeTag);
         const isText = this.attachmentModal?.isText(attachment.attachMimeTag);
+        const isEml = this.attachmentModal?.isPreviewableEml(attachment.attachMimeTag);
 
         // Icon selection helper
         const getIcon = () => {
@@ -222,6 +223,10 @@ export class MessageContentRenderer {
             } else if (isText) {
                 return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 attachment-icon">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                </svg>`;
+            } else if (isEml) {
+                return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-blue-500">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                                 </svg>`;
             }
             return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 attachment-icon">
